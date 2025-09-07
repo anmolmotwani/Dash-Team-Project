b38dd7cecc8c9feb8a2027c22fda245fd26dacfb
ChristopherAGoodwin <cagoodwin@wm.edu>
2025-09-07 10:05:54 -0400

    Added more to page1.py about using the open-meteo api. currently remains untested. added a lot of notes

--- project/pages/page1.py
+++ project/pages/page1.py
@@ -1,7 +1,10 @@
 import dash
 from dash import Dash, dcc, Input, html, Output, callback
 from geopy.geocoders import Nominatim
-import openmeteo_requests
+import openmeteo_requests 
+from retry_requests import retry
+import requests_cache
+
 
 dash.register_page(__name__, path = "/page1", name = "Weather Report")
 ## On this page we will have a layout for a user to first input the location
@@ -10,11 +13,45 @@ dash.register_page(__name__, path = "/page1", name = "Weather Report")
 ## Allow users to convert from Farenheit to Celsius and vice versa
 
 
+##taken directly from the open-meteo API. Do we keep this here???
+cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+openmeteo = openmeteo_requests.Client(session = retry_session)
 
+##setting parameters
+
+##Variables we want to allow the user to control
+##latitude and longitude via Location (input a city/country/etc)
+##farenheit vs celsius
+
+url = "https://api.open-meteo.com/v1/forecast"
+params = {
+    #"latitude": function to recieve latitude from input
+    #"longitude": function to recieve longitude from input
+    #"daily" : ["temperature_2m_max", "percipitation_sum",...]
+    #"hourly":["temperature_2m","rain",...] This is where we will put in all the parameters for weather information
+    #we want to return to the user
+    #"current": ["temperature_2m","rain",...]
+    
+    
+    #"temperature_unit": user_input (default to fahrenheit) but allow user to change to celsius.
+
+    #"past_days":7
+    
+    #"timezone" : "America/New_York" (Sets time zone to EST, our timezone)
+}
+
+##returned a two tiered dictionary of seperated by days? and conditions
+
+responses = openmeteo.weather_api(url, params = params)
+
+
+##response = responses[0]
 
 layout = html.Div(
     
     [
+    
     ]
 )
 
--- testing.py
+++ testing.py
@@ -4,5 +4,19 @@ from geopy.geocoders import Nominatim
 
 place_finder = Nominatim(user_agent = "my_user_agent")
 
-result = place_finder.geocode("Berlin", addressdetails=True)
-print(f"Latitude: {result.latitude} \nLongitude: {result.longitude}")
+#result = place_finder.geocode("Berlin", addressdetails=True)
+#print(f"Latitude: {result.latitude} \nLongitude: {result.longitude}")
+#cityTest = input("Put in a city: ")
+#result = place_finder.geocode(cityTest, addressdetails=True)
+#print(f"Latitude: {result.latitude} \nLongitude: {result.longitude}")
+
+##We could try llimiting the scope to only the US?
+
+def getLatLong (cityX, countryY):
+    ##if we want to restrict to only USA we could add country_codes=us
+    cityLatLong = place_finder.geocode({'city':cityX, 'country':countryY}, addressdetails=True)
+    return(cityLatLong.latitude, cityLatLong.longitude)
+
+x = input("City: ")
+y = input("Country: ")
+print(getLatLong(x,y))
