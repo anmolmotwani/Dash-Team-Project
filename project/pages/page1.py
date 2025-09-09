44ba8197439942ae633f152f512e79c6c36867f4
ChristopherAGoodwin <cagoodwin@wm.edu>
2025-09-09 14:31:39 -0400

    added a slider added date and time adjustment

--- project/pages/page1.py
+++ project/pages/page1.py
@@ -4,6 +4,7 @@ from geopy.geocoders import Nominatim
 import requests_cache
 from retry_requests import retry
 import openmeteo_requests
+import pandas as pd
 from datetime import date
 
 # ----- Initialize geopy and OpenMeteo -----
@@ -47,6 +48,8 @@ layout = html.Div(className="weatherPage clear", children=[
     
     html.Div(id="GetWeather", className="main-card"),
     
+    html.Div(dcc.Slider(0,23,1, id = "TimeSlider")),
+    
     html.Div(id="forecast-cards", style={"display":"flex","overflowX":"auto","padding":"10px"})
 ])
 
@@ -57,13 +60,15 @@ layout = html.Div(className="weatherPage clear", children=[
     Input("inputCity","value"),
     Input("inputCountry","value"),
     Input("TempSetting","value"),
-    Input("paramSettings","value")
+    Input("paramSettings","value"),
+    Input("TimeSlider", "value")
 )
-def update_weather(city, country, tempUnit, params):
+def update_weather(city, country, tempUnit, params, time):
     try:
         # Get lat/lon
         location = placeFinder.geocode({'city':city,'country':country})
         lat, lon = location.latitude, location.longitude
+        
         
         # Placeholder for API response
         current = {
@@ -71,6 +76,34 @@ def update_weather(city, country, tempUnit, params):
             "Humidity": "60%",
             "Rain": "10%"
         }
+        
+        apiParams = {
+                    'latitude':lat,
+        'longitude':lon,
+        'hourly': ['temperature_2m','precipitation','relative_humidity_2m'],
+        'temperature_unit':'fahrenheit',
+        'past_days':3,
+        }
+        responses = openmeteo.weather_api(url = "https://api.open-meteo.com/v1/forecast", params = apiParams)
+        response = responses[0]
+        hourly = response.Hourly[0]
+        hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+        hourly_precipitation = hourly.Variables(1).ValuesAsNumpy()
+        hourly_relative_humidity_2m = hourly.Variables(2).ValuesAsNumpy()
+         
+        hourly_data = {"date": pd.date_range(
+	    start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
+	    end = pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
+	    freq = pd.Timedelta(seconds = hourly.Interval()),
+	    inclusive = "left"
+        )}
+        
+        hourly_data["temperature_2m"] = hourly_temperature_2m
+        hourly_data["precipitation"] = hourly_precipitation
+        hourly_data["hourly_relative_humidity_2m"] = hourly_relative_humidity_2m
+         
+        hourly_dataframe = pd.DataFrame(data = hourly_data)
+        hourly_list = hourly_dataframe.to_numpy()
         
         # Choose background class
         bg_class = "clear"
@@ -82,9 +115,9 @@ def update_weather(city, country, tempUnit, params):
         # Current weather card
         main_card = html.Div(className=f"main-card {bg_class}", children=[
             html.H2(f"{city}, {country}"),
-            html.P(f"Temperature: {current['Temperature']}"),
-            html.P(f"Humidity: {current['Humidity']}"),
-            html.P(f"Rain: {current['Rain']}")
+            html.P(f"Temperature: {hourly_list[0][1]}"),
+            html.P(f"Humidity: {hourly_list[0][3]}"),
+            html.P(f"Rain: {hourly_list[0][2]}")
         ])
         
         # Forecast cards placeholder
